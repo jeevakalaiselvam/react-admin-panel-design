cfe7b6e424b7fa18237517d6202ef58dd1f5ce77
Jeeva Kalaiselvam <77735712+jeevakalaiselvam@users.noreply.github.com>
2021-06-19 06:01:05 +0530

    Edit user completed

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,3 +1,12 @@
+import {
+    CalendarToday,
+    Email,
+    EmailOutlined,
+    LocationSearching,
+    PermIdentity,
+    PhoneAndroid,
+    Publish,
+} from "@material-ui/icons";
 import React from "react";
 import "./user.css";
 
@@ -20,14 +29,110 @@ export default function User() {
                             <span className="userShowName">
                                 Patrick Steward
                             </span>
-                            <span className="userShowTitle">
+                            <span className="userShowTitleTop">
                                 Software Developer
                             </span>
                         </div>
                     </div>
-                    <div className="userShowBottom"></div>
+                    <div className="userShowBottom">
+                        <span className="userShowTitle">Account Details</span>
+                        <div className="userShowInfo">
+                            <PermIdentity className="userShowIcon" />
+                            <span className="userShowInfoTitle">patrick11</span>
+                        </div>
+                        <div className="userShowInfo">
+                            <CalendarToday className="userShowIcon" />
+                            <span className="userShowInfoTitle">
+                                10.12.1999
+                            </span>
+                        </div>
+                        <span className="userShowTitle">Contact Details</span>
+                        <div className="userShowInfo">
+                            <PhoneAndroid className="userShowIcon" />
+                            <span className="userShowInfoTitle">
+                                +1 123 456 67
+                            </span>
+                        </div>
+                        <div className="userShowInfo">
+                            <EmailOutlined className="userShowIcon" />
+                            <span className="userShowInfoTitle">
+                                patrick11@gmail.com
+                            </span>
+                        </div>
+                        <div className="userShowInfo">
+                            <LocationSearching className="userShowIcon" />
+                            <span className="userShowInfoTitle">
+                                New York | USA
+                            </span>
+                        </div>
+                    </div>
                 </div>
-                <div className="userUpdate"></div>
+                <div className="userUpdate">
+                    <span className="userUpdateTitle">Edit</span>
+                    <form className="userUpdateForm">
+                        <div className="userUpdateLeft">
+                            <div className="userUpdateItem">
+                                <label>Username</label>
+                                <input
+                                    type="text"
+                                    placeholder="patrick11"
+                                    className="userUpdateInput"
+                                />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Full Name</label>
+                                <input
+                                    type="text"
+                                    placeholder="Patrick Steward"
+                                    className="userUpdateInput"
+                                />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Email</label>
+                                <input
+                                    type="text"
+                                    placeholder="patrick11@gmail.com"
+                                    className="userUpdateInput"
+                                />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Phone</label>
+                                <input
+                                    type="text"
+                                    placeholder="+1 123 456 67"
+                                    className="userUpdateInput"
+                                />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Address</label>
+                                <input
+                                    type="text"
+                                    placeholder="New York | USA"
+                                    className="userUpdateInput"
+                                />
+                            </div>
+                        </div>
+                        <div className="userUpdateRight">
+                            <div className="userUpdateUpload">
+                                <img
+                                    src="https://cdn.fastly.picmonkey.com/contentful/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=800&q=70"
+                                    alt=""
+                                    className="userUpdateImg"
+                                />
+                                <label htmlFor="file">
+                                    <Publish className="userUpdateIcon" />
+                                </label>
+                                <input
+                                    type="file"
+                                    id="file"
+                                    style={{ display: "none" }}
+                                />
+                            </div>
+
+                            <button className="userUpdateButton">Update</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
